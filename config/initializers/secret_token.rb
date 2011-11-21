eae80b5366f7eca40e13366f280bab6a181cd640
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-11-21 12:08:52 +0100

    Initial empty rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TracketyTrack::Application.config.secret_token = 'bbc8abffeaf9d5036ea56182c65acb3162a90c02745fe5311e86d26db62f88594ca7754697adaa85f36b44c84e376554b13e6f1453633a29708bfb7b1616dab9'
