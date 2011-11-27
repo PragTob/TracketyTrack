51c6ab315ecdc0adb327606ff40d6d30d39ca867
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-11-27 23:22:31 +0100

    Annotated the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,15 @@ class User < ActiveRecord::Base
                     :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i},
                     :uniqueness => true, :on => :create
 end
+# == Schema Information
+#
+# Table name: users
+#
+#  id          :integer         not null, primary key
+#  name        :string(255)
+#  email       :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,3 +36,15 @@ describe User do
     it{should_not be_valid}
   end
 end
+# == Schema Information
+#
+# Table name: users
+#
+#  id          :integer         not null, primary key
+#  name        :string(255)
+#  email       :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
