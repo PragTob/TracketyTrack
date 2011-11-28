fbd599fda6f403c260798a3f371b00f5ef07b7de
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-11-28 17:40:10 +0100

    Validating the presence of user story names

--- spec/models/user_story_spec.rb
+++ spec/models/user_story_spec.rb
@@ -1,9 +1,29 @@
 require 'spec_helper'
 
 # more examples to come when the model gets more complex
-#describe UserStory do
-#  pending "add some examples to (or delete) #{__FILE__}"
-#end
+describe UserStory do
+  before :each do
+    @user_story = Factory.build(:user_story)
+  end
+
+  subject{@user_story}
+
+  it { should respond_to :name }
+  it { should respond_to :description }
+  it { should respond_to :acceptance_criteria }
+  it { should respond_to :priority }
+  it { should respond_to :estimation }
+  it { should be_valid }
+
+  it "is invalid without a name" do
+    UserStory.new.should_not be_valid
+  end
+
+  it "needs a name to be valid" do
+    UserStory.new(name: "Work! Work!").should be_valid
+  end
+
+end
 
 # == Schema Information
 #
