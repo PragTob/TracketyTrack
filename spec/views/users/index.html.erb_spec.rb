369fbd579a303dd5e23f7232acf3fb5f093eee4b
FlorianWestphal <florian.westphal.1988@googlemail.com>
2011-11-28 17:09:42 +0100

    mocks replaced with users factory in user's view tests

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -2,27 +2,17 @@ require 'spec_helper'
 
 describe "users/index.html.erb" do
   before(:each) do
-    assign(:users, [
-      stub_model(User,
-        :name => "Name",
-        :email => "Email",
-        :description => "MyText"
-      ),
-      stub_model(User,
-        :name => "Name",
-        :email => "Email",
-        :description => "MyText"
-      )
-    ])
+    @user = Factory(:user)
+    assign(:users, [@user])
   end
 
   it "renders a list of users" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => @user.name.to_s, :count => 1
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    assert_select "tr>td", :text => @user.email.to_s, :count => 1
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => @user.description.to_s, :count => 1
   end
 end
