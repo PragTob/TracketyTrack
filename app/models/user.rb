b4c9a6f1b1d281ffd4d010b371324262d3dc1457
FlorianWestphal <florian.westphal.1988@googlemail.com>
2011-11-28 17:50:41 +0100

    hash format changed to new format in user and project model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
   has_many :projects
 
-  validates :name,  :presence => true
-  validates :email, :presence => true,
-                    :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i},
-                    :uniqueness => true, :on => :create
+  validates :name,  presence: true
+  validates :email, presence: true,
+                    format: {with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i},
+                    uniqueness: true,
+                    on: :create
 end
 # == Schema Information
 #
