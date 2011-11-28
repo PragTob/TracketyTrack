e2821c137b32e817ebaf25e3e065c8801de1baca
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-11-28 11:05:05 +0100

    A bit of userspec refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,49 +4,37 @@ require 'spec_helper'
 describe User do
 
   before(:each) do
-    @user1 = Factory.build(:user)
-    @user2 = Factory.build(:user)
+    @user = Factory.build(:user)
   end
 
-  def valid_attributes
-    {name: "Peter", email: "peterinchen@example.com"}
-  end
-
-  subject{@user1}
+  subject{@user}
 
   it{should respond_to :name}
   it{should respond_to :email}
   it{should respond_to :description}
 
-  it "can be created with valid attributes" do
-    User.new(valid_attributes).should be_valid
+  context "given valid attributes" do
+    it { should be_valid }
   end
 
-  it "is invalid without a name" do
-    User.new(valid_attributes.merge(name: nil)).should_not be_valid
+  context "without a name" do
+    before { @user.name = nil }
+    it { should_not be_valid }
   end
 
-  context "when name is not given" do
-    before{@user1.name = nil}
-    after{@user1.name = @user2.name}
-    it{should_not be_valid}
+  context "without an email" do
+    before { @user.email = nil }
+    it { should_not be_valid }
   end
 
-  context "when email is not given" do
-    before{@user1.email = nil}
-    after{@user1.email = @user2.email}
-    it{should_not be_valid}
-  end
 
   context "when email has not the correct format" do
-    before{@user1.email = "florian"}
-    after{@user1.email = @user2.email}
+    before{@user.email = "florian"}
     it{should_not be_valid}
   end
 
   context "when email is already taken by an other user" do
-    before{@user2.save}
-    after{@user2.destroy}
+    before{Factory(:user)}
     it{should_not be_valid}
   end
 
@@ -56,18 +44,18 @@ describe User do
     end
 
     it "should have empty roles upon creation" do
-      @user1.roles.should be_empty
+      @user.roles.should be_empty
     end
 
     it "can have a role assigned" do
-      @user1.roles << @role
-      @user1.roles.size.should == 1
-      @user1.should be_valid
+      @user.roles << @role
+      @user.roles.size.should == 1
+      @user.should be_valid
     end
 
     it "can have multiple roles assigned" do
-      @user1.roles << @role << Factory.build(:other_role)
-      @user1.roles.size.should == 2
+      @user.roles << @role << Factory.build(:other_role)
+      @user.roles.size.should == 2
     end
 
   end
