a3c86068d497577feca5e29841a4af47a5f687a8
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-11-30 17:51:18 +0100

    User.authenticate works

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,12 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
 
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
   private
 
     def encrypt_password
