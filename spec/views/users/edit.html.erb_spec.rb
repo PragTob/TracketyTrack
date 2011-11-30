e14989280c2d6c2ef731320f60133eb6583a3eef
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-11-30 17:30:00 +0100

    now the form has a password anf a password confirmation field

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -10,9 +10,12 @@ describe "users/edit.html.erb" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => users_path(@user), :method => "post" do
-      assert_select "input#user_name", :name => "user[name]"
-      assert_select "input#user_email", :name => "user[email]"
-      assert_select "textarea#user_description", :name => "user[description]"
+      assert_select "input#user_name", name: "user[name]"
+      assert_select "input#user_email", name: "user[email]"
+      assert_select "textarea#user_description", name: "user[description]"
+      assert_select "input#user_password", name: "user[password]"
+      assert_select "input#user_password_confirmation", name: "user[password_confirmation"
     end
   end
 end
+
