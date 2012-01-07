8c0bb3542717bf2ba3e6deeb0484b9246731fcf7
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2012-01-07 20:10:40 +0100

    Deleting stuff feels good. This all was unnecessary scaffold, so removed controller actions, views and specs. It's easier to recover than maintain.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,36 +1,5 @@
 class CommentsController < ApplicationController
 
-  def index
-    @comments = Comment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @comments }
-    end
-  end
-
-  def show
-    @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @comment }
-    end
-  end
-
-  def new
-    @comment = Comment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @comment }
-    end
-  end
-
-  def edit
-    @comment = Comment.find(params[:id])
-  end
-
   def create
     @comment = Comment.new(params[:comment])
     @comment.date = DateTime.now
@@ -39,23 +8,9 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to @comment.user_story
     else
-      render action: "new"
+      redirect_to request.referrer, error: "Something went wrong with saving the comment"
     end
 
-  end
-
-  def update
-    @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      if @comment.update_attributes(params[:comment])
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def destroy
@@ -65,5 +20,6 @@ class CommentsController < ApplicationController
 
     redirect_to user_story
   end
+  
 end
 
