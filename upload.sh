018a014befd4230ecb3970a93e6750ea79d23305
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2012-01-10 01:35:34 +0100

    upload.sh modified to commit before pushing ;-)

--- upload.sh
+++ upload.sh
@@ -1,6 +1,8 @@
 git merge master
 RAILS_ENV=production bundle exec rake assets:precompile --trace
 rspec
+git add .
+git commit -am "Freshly compiled assets for your heroku pleasure"
 git push origin heroku_assets
 git push heroku heroku_assets:master
 
