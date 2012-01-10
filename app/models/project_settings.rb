571a50bba028abaa4b8fd5f3d489a51b5ea0fa6d
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2012-01-10 15:27:12 +0100

    Project settings annotations

--- app/models/project_settings.rb
+++ app/models/project_settings.rb
@@ -63,3 +63,15 @@ class ProjectSettings < ActiveRecord::Base
 
 end
 
+# == Schema Information
+#
+# Table name: project_settings
+#
+#  id                  :integer         not null, primary key
+#  travis_ci_repo      :string(255)
+#  travis_last_updated :datetime
+#  project_id          :integer
+#  created_at          :datetime        not null
+#  updated_at          :datetime        not null
+#
+
--- spec/models/project_settings_spec.rb
+++ spec/models/project_settings_spec.rb
@@ -89,3 +89,15 @@ describe ProjectSettings do
 
 end
 
+# == Schema Information
+#
+# Table name: project_settings
+#
+#  id                  :integer         not null, primary key
+#  travis_ci_repo      :string(255)
+#  travis_last_updated :datetime
+#  project_id          :integer
+#  created_at          :datetime        not null
+#  updated_at          :datetime        not null
+#
+
