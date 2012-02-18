adb51b8c9cf1cc3cbdf06a7aee9fb5874553ce4a
Josefine Harzmann <jharzmann@gmail.com>
2012-02-19 00:14:53 +0100

    added tests for statistics helper

--- spec/helpers/statistics_helper_spec.rb
+++ spec/helpers/statistics_helper_spec.rb
@@ -23,5 +23,59 @@ describe StatisticsHelper do
 
   end
 
+  describe "get_completed_story_points" do
+
+    it "returns an empty string if there are no units" do
+      get_completed_story_points({}).should eq ""
+    end
+
+    it "returns value of unit as string" do
+      completed_story_points_per_unit = {1 => 10}
+      get_completed_story_points(completed_story_points_per_unit).should eq "10"
+    end
+
+    it "returns all values as string, added up and separated by a comma" do
+      completed_story_points_per_unit = {1 => 10, 2 =>15, 3 => 20}
+      get_completed_story_points(completed_story_points_per_unit).should eq "10,25,45"
+    end
+
+  end
+
+  describe "get_legend_dates" do
+
+    it "returns an empty string if there are no units" do
+      get_legend_dates({}).should eq ""
+    end
+
+    it "returns value of unit as string" do
+      completed_story_points_per_unit = {1 => 10}
+      get_legend_dates(completed_story_points_per_unit).should eq "1"
+    end
+
+    it "returns all values as string, separated by a streak" do
+      completed_story_points_per_unit = {1 => 10, 2 =>15, 3 => 20}
+      get_legend_dates(completed_story_points_per_unit).should eq "1|2|3"
+    end
+
+  end
+
+  describe "get_line_dates" do
+
+    it "returns an empty string if there are no units" do
+      get_line_dates({}).should eq ""
+    end
+
+    it "returns value of unit as string" do
+      completed_story_points_per_unit = {1 => 10}
+      get_line_dates(completed_story_points_per_unit).should eq "10"
+    end
+
+    it "returns all values as string, separated by a comma" do
+      completed_story_points_per_unit = {1 => 10, 2 =>15, 3 => 20}
+      get_line_dates(completed_story_points_per_unit).should eq "10,15,20"
+    end
+
+  end
+
 end
 
